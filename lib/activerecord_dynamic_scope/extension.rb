33bb286021e22d20fe5ce5c8fe3b228f4597c6f5
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-20 17:49:59 +0900

    Add frozen_string_literal: true

--- lib/activerecord_dynamic_scope/extension.rb
+++ lib/activerecord_dynamic_scope/extension.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ActiveRecordDynamicScope
   module Extension
     extend ActiveSupport::Concern
--- lib/activerecord_dynamic_scope/railtie.rb
+++ lib/activerecord_dynamic_scope/railtie.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ActiveRecordDynamicScope
   class Railtie < Rails::Railtie
     ActiveSupport.on_load :active_record do
